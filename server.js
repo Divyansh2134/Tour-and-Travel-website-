83010f381ebb5c9e8c79e9f9d7d7c20f86652ea3
Divyansh sharma <divyanshsharma2134@gmail.com>
2022-03-13 23:52:26 +0530

    fifth

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/',function(req,res){
 
 
 
-app.listen(process.env.PORT || 3000,function(){
+var server=app.listen(process.env.PORT || 3000,function(){
   console.log("server is running")
 })
